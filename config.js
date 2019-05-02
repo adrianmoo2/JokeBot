403fd76ca9fe8812592b9c6f537786d8d2ad3088
Adrian_Tran <atran059@ucr.edu>
2019-05-02 10:54:30 -0700

    Added configjs

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports={
+  consumer_key:         'oUa8jOczGHdbwqy4Qss1BoEtu',
+  consumer_secret:      'b0ZYajwqonzahrTGyXAmsFgjXLQXRMD2RLY44ErZSbxfo1Qecr',
+  access_token:         '1118627419856859137-hQc6cLZRWHfogzlYaaW1zbFZqi4zgj',
+  access_token_secret:  'vL5RNJbvwxVuvuaQrjCJ6NuQvSqv2H9lp2PN7mfIyM4oR'
+}
