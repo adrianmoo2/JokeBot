452d32ef5c8a47ddf253eb4018e8a6f642be2919
akiyowind <atran059@ucr.edu>
2019-06-02 13:28:01 -0700

    Fixed interval back to 12 hrs

--- JokeBot.js
+++ JokeBot.js
@@ -8,8 +8,7 @@ var jokeTweet = "ss";
 var index = Math.floor(Math.random()*3);
 
 //tweetJoke();
-//setInterval(tweetJoke, 1000*60*60*12);
-setInterval(tweetJoke, 1000*60);
+setInterval(tweetJoke, 1000*60*60*12);
 
 function tweetJoke() {
     function whichToTweet(callback){
